2dabbc9a68ea5524e5c8e20d0eee21f2f89903c4
rohacsidana <rohacsi.dana@gmail.com>
2022-04-06 09:58:56 +0200

    alapfüggvények létrehozása

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", init)
+function ID(elem) {
+    return document.getElementById(elem)
+}
+function $(elem){
+    return document.querySelectorAll(elem) //tömb
+}
+function $1(elem){
+    return document.querySelector(elem) //ha egy van belőle
+}
+function init(){
+    
+}
